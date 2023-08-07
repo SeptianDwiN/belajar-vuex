08f4faabd91b65c5bd410cf025c503f5772f620f
SeptianDwiN <21221753149@smkassalaambandung.sch.id>
2023-08-07 13:59:58 +0700

    belajar vuex

---
+++ src/store/modules/donatur.js
@@ -0,0 +1,21 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            {name: "Septian"},
+            {name: "Dimas"},
+            {name: "Ikbal"},
+            {name: "Amalia"},
+            {name: "Dimas"},
+            {name: "Fitri"},
+        ],
+    },
+    mutations: {
+        //NONE
+    },
+    actions: {
+        //NONE
+    },
+};
+
+export default donatur;
