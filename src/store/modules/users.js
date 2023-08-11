a3060336586f1e08f258f3478dc9a5de4c94357c
SeptianDwiN <21221753149@smkassalaambandung.sch.id>
2023-08-11 14:36:27 +0700

    add 3 file

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -20,11 +20,31 @@ const users = {
         console.log(error);
       }
     },
+
+    async createUser ({ commit },userData){
+      try{
+        const response = await axios.post(
+          "https://fakestoreapi.com/users",
+          userData
+        );
+        commit("ADD_USER", response.data);
+        return response.data;
+      }catch (error){
+        alert(error);
+        console.log(error);
+      }
+    }
   },
+ 
+
+
   mutations: {
     SET_USERS(state, users) {
       state.userData = users;
     },
+    ADD_USER(state, user) {
+      state.addData.push(user)
+    }
   },
 };
 
